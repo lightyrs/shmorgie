2cb8ac1e9a1f0fceec6f175cd885b6cfc6a84416
Harris Novick <harrisdavidnovick@gmail.com>
2015-07-26 20:23:25 -0400

    create commits from received github payloads

--- app/models/receivers/github_receiver.rb
+++ app/models/receivers/github_receiver.rb
@@ -8,7 +8,20 @@ module Receivers
     end
 
     def process_inbound_message(payload)
-      Rails.logger.debug(payload)
+      branch  = payload['ref'].split("refs/heads/").last
+      commits = payload['commits'].sort_by { |commit| DateTime.parse(commit['timestamp']) }
+
+      commits.each do |commit_object|
+        Commit.find_or_create_by(sha: commit_object['id']) do |commit_model|
+          commit_model.user         = @owner
+          commit_model.repo         = @repo
+          commit_model.branch       = branch
+          commit_model.author       = commit_object.fetch('author', {})['name']
+          commit_model.author_email = commit_object.fetch('author', {})['email']
+          commit_model.message      = commit_object['message']
+          commit_model.url          = commit_object['url']
+        end
+      end
     end
   end
 end
