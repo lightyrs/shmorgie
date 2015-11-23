1e0f7b115f7e7a917dacf41481ee31ccf4c8385c
Harris Novick <harrisdavidnovick@gmail.com>
2015-11-22 23:54:47 -0500

    Improving reddit crawler

--- app/models/clients/reddit_client.rb
+++ app/models/clients/reddit_client.rb
@@ -20,13 +20,13 @@ module Clients
       subreddits_from_multi('evilnight', 'truemusic')
     end
 
-    def get_hot_from_defaults
+    def get_new_from_defaults
       unsorted = default_subreddits.map do |sub|
-        hot_links(sub).tap do |hot|
+        new_links(sub).tap do |hot|
           sleep 3
         end
       end
-      unsorted.flatten.sort_by { |link| link[:score] }.reverse!
+      sorted = unsorted.flatten.sort_by { |link| link[:score] }.reverse!
     end
 
     # @option params [String] :after Return results after the given
@@ -41,30 +41,36 @@ module Clients
     #   time period to consider when sorting.
 
     def new_links(subreddit = 'all', options = {})
-      options.merge!(limit: 100)
+      { t: :hour }.merge!(options)
       links = @client.get_new(subreddit, options)
       links.map { |link| format_link(link) }
     end
 
     def hot_links(subreddit = 'all', options = {})
-      options.merge!(limit: 100)
+      { t: :hour }.merge!(options)
       links = @client.get_hot(subreddit, options)
       links.map { |link| format_link(link) }
     end
 
     def top_links(subreddit = 'all', options = {})
-      options.merge!(limit: 100)
+      { t: :hour }.merge!(options)
       links = @client.get_top(subreddit, options)
       links.map { |link| format_link(link) }
     end
 
     private
 
+    def post_link_to_tumblr(link)
+      @tumblr_client ||= Clients::TumblrClient.new
+      @tumblr_client.make_video_post(url: link.url, caption: link.attribution, tags: link.tags)
+    end
+
     def format_link(link)
       {
+        raw: link.to_h,
         score: link.score.try(:to_i),
         title: link.title,
-        tags: [map_domain(link.try(:domain)), link.subreddit],
+        tags: [map_domain(link.try(:domain)), link.subreddit].compact,
         url: link.try(:url),
         attribution: "https://www.reddit.com/user/#{link.author} posted this to https://www.reddit.com#{link.permalink} at #{Time.at(link.created_utc).utc}",
         post_type: link.try(:post_hint)
@@ -82,7 +88,7 @@ module Clients
         "Vimeo"
       when domain.match(/facebook/i)
         "Facebook"
-      when domain.match(/\Aself\./i)
+      when domain.match(/\Aself\.|reddit\.com/i)
         "Reddit"
       when domain.match(/spotify/i)
         "Spotify"
@@ -90,6 +96,34 @@ module Clients
         "Bandcamp"
       when domain.match(/mixcloud/i)
         "Mixcloud"
+      when domain.match(/imgur/i)
+        "Imgur"
+      when domain.match(/wikipedia/i)
+        "Wikipedia"
+      when domain.match(/bbc\.com|bbc\.co\.uk/i)
+        "BBC"
+      when domain.match(/stereogum/i)
+        "Stereogum"
+      when domain.match(/pitchfork/i)
+        "Pitchfork"
+      when domain.match(/noisey\.vice\.com/i)
+        "Noisey"
+      when domain.match(/npr\.org/i)
+        "NPR"
+      when domain.match(/tumblr\.com/i)
+        "Tumblr"
+      when domain.match(/dailymotion\.com/i)
+        "Dailymotion"
+      when domain.match(/itunes\.apple\.com/i)
+        "iTunes"
+      when domain.match(/myspace\.com/i)
+        "Myspace"
+      when domain.match(/flickr\.com/i)
+        "Flickr"
+      when domain.match(/twitter\.com/i)
+        "Twitter"
+      when domain.match(/reddit\.com/i)
+
       else
         domain
       end
