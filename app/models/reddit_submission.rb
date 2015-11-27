f7a4b0e306976902664d18f13ccc2c41e0c70e7d
Harris Novick <harrisdavidnovick@gmail.com>
2015-11-27 03:08:17 -0500

    Fix hash access

--- app/models/reddit_submission.rb
+++ app/models/reddit_submission.rb
@@ -1,6 +1,6 @@
 class RedditSubmission < ActiveRecord::Base
 
-  attr_accessible :fullname, :submitted_at_utc
+  attr_accessible :fullname, :submitted_at_utc, :reposted_at
 
   validates :fullname, presence: true, uniqueness: true
 end
