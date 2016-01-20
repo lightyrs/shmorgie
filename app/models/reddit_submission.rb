4f744727cc0a2b4b9513fad4e46ba343248c1686
Harris Novick <harrisdavidnovick@gmail.com>
2016-01-19 22:45:01 -0500

    Add #subreddit to attr_accessible list

--- app/models/reddit_submission.rb
+++ app/models/reddit_submission.rb
@@ -1,6 +1,6 @@
 class RedditSubmission < ActiveRecord::Base
 
-  attr_accessible :fullname, :title, :submitted_at_utc, :reposted_at
+  attr_accessible :fullname, :title, :subreddit, :submitted_at_utc, :reposted_at
 
   validates :fullname, presence: true, uniqueness: true
 end
